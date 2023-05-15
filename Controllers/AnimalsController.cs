73ead38d7c65f8a805171fddee3ca0e013d97c7b
Tim Holleque <timothy.t.holleque@gmail.com>
2023-05-15 08:48:32 -0400

    initial

---
+++ Controllers/AnimalsController.cs
@@ -0,0 +1,20 @@
+﻿using AdoptionMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdoptionMVC.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        private readonly AnimalsDbContext _context;
+        public AnimalsController(AnimalsDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/AnimalsDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdoptionMVC.Models;
+
+public partial class AnimalsDbContext : DbContext
+{
+    public AnimalsDbContext()
+    {
+    }
+
+    public AnimalsDbContext(DbContextOptions<AnimalsDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Animal> Animals { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=AnimalsDb;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Animal>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Animals__3214EC079270A5DD");
+
+            entity.Property(e => e.Breed).HasMaxLength(30);
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.Img)
+                .HasMaxLength(255)
+                .HasColumnName("img");
+            entity.Property(e => e.Name).HasMaxLength(25);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
